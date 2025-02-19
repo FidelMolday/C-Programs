7623e2d5e644bd7b6bac15979832b9db6061c8a5
Molday Developer <126654078+FidelMolday@users.noreply.github.com>
2025-02-19 11:29:35 +0300

    Add files via upload

---
+++ csharppractise/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int i = 1;
+        while (i <= 5)
+        {
+            Console.WriteLine($"Count: {i}");
+            i++;
+        } // Removed unnecessary semicolon here
+    }
+}
