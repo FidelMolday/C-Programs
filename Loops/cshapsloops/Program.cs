d84e91dfd0294e7500b9aa3beb87351a9c02aea0
Molday Developer <126654078+FidelMolday@users.noreply.github.com>
2025-02-25 11:14:21 +0300

    Add files via upload

---
+++ Loops/cshapsloops/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int num = 1;
+        while (num <= 10)
+        {
+            Console.WriteLine(num);
+            num++;
+        }
+    }
+}
---
+++ Loops/oddoreven/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine()); // 'Convert' is correctly capitalized
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine(number + " is an even number");
+        }
+        else
+        {
+            Console.WriteLine(number + " is an odd number");
+        }
+    }
+}
